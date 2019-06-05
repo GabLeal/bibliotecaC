71094f5b2bc3e8fb578f236d622be2dcc309e310
LealDev <gabriellealttrj@gmail.com>
2019-06-04 22:59:09 -0300

    Rotinas de livros finalizadas

--- principal.c
+++ principal.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct Livro{
     int id;
@@ -8,27 +9,11 @@ typedef struct Livro{
     int quantidade;
 }Livro;
 
-/*   Livro vetor_livro[2];
-    printf("CADASTRANDO LIVROS\n \n");
-    for (int i = 0; i < 2; i++){
-        printf("Nome do livro: ");
-        scanf(" %[^\n]",vetor_livro[i].nome);
-        printf("Quantidade: ");
-        scanf("%d",&vetor_livro[i].quantidade);
-    }
-    printf("\n");
-    printf("CONUSLTANDO TODOS OS LIVROS \n \n");
-    for (int i = 0; i < 2; i++) {
-        printf("Nome: %s \n",vetor_livro[i].nome);
-        printf("Quantidade: %d\n",vetor_livro[i].quantidade);
-
-        printf("\n \n");
-    }*/
-
 void cadastroLivro(int livrosCadastrados, Livro livros[],int maxLivros[]){
         //livros = (Livro *) realloc(livros, livrosCadastrados*sizeof(Livro));
         
         livros[livrosCadastrados].id = livrosCadastrados;
+        
         printf("Nome do livro: ");
         scanf(" %[^\n]",livros[livrosCadastrados].nome);
         printf("Quantidade: ");
@@ -50,7 +35,7 @@ void consultarLivros(int livrosCadastrados, Livro livros[]){
 void retirarLivros(Livro livro[], int livrosCadastrados, int opc, int maxLivros[]){
     int id;
     int cont = 0;
-
+     printf("\e[H\e[2J");
     if (opc==3){
         printf("Digite o ID do livro que sera retirado: ");
         scanf("%d",&id);
@@ -58,14 +43,17 @@ void retirarLivros(Livro livro[], int livrosCadastrados, int opc, int maxLivros[
         for (int i = 0; i <= livrosCadastrados; i++){
             if (id == livro[i].id){
                 livro[i].quantidade -= 1;
-                printf("Retirada efetuada com sucesso! \n");
+                printf("Retirada efetuada com sucesso... \n");
                 cont++;
+                sleep(1.5);
+                printf("\e[H\e[2J");
             }
 
         }
 
         if (cont==0){
-            printf("O livro solicitado não existe em nossos registros. \n");
+            printf("O livro solicitado não existe em nossos registros... \n");
+            sleep(1.5);
         }
 
     }else{
@@ -73,19 +61,27 @@ void retirarLivros(Livro livro[], int livrosCadastrados, int opc, int maxLivros[
         scanf("%d",&id);
         
         for (int i = 0; i <= livrosCadastrados; i++){
-            if (id == livro[i].id && livro[i].quantidade < maxLivros[i]){
-                livro[i].quantidade += 1;
-                printf("Devolução efetuada com sucesso! \n");
+            if (id == livro[i].id){
+                
+                if (livro[i].quantidade < maxLivros[i]){
+                    livro[i].quantidade += 1;
+                    printf("Devolução efetuada com sucesso... \n");
+                }else{
+                    printf("Ops! Todos as edições desse livro já foram devolvidas. Tente novamente... \n");
+                }
+                
                 cont++;
             }
 
         }
 
         if (cont==0){
-            printf("O livro solicitado não existe em nossos registros ou você está excedendo o limite.\n");
+            printf("O livro solicitado não existe em nossos registros...\n");
+            
         } 
     }
-
+    sleep(1.5);
+    printf("\e[H\e[2J");
 }
 
 
@@ -100,6 +96,7 @@ int main (){
     //livros = (Livro *) malloc(livrosCadastrados * sizeof(Livro)); 
     
     do{
+        
         printf("1-Cadastrar livro \n");
         printf("2-Consultar livro \n");
         printf("3-Retirar livro \n");
